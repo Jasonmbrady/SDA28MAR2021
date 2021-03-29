4be33870ddb826538675475cfe528f30be426978
Jason Brady <jasonbrady@Jasons-MacBook-Pro.local>
2021-03-29 09:13:21 +0300

    Initial Commit

---
+++ wk1/day1.js
@@ -0,0 +1,24 @@
+// Probelm 1
+
+/*
+    Recreate the .push method without using the built-in .push
+        Given an array and a new item, add the item to the end of the array
+        return the new length of the array
+*/
+
+function push(arr, newItem){
+    //Code Here
+}
+
+
+// Problem 2
+
+/* 
+    Nth to last
+        Given an array and an integer, which represents the position starting from the back (last element) of the array,
+        return the nth-to-last element.
+*/
+
+function nthToLast(arr, int){
+    //Code Here
+}
