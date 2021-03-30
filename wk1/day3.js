c8f112d2304368e0f8c942351ec4318465cdd720
Jason M Brady <jasonmbrady42@gmail.com>
2021-03-30 09:18:32 +0300

    Now with actual code
    '

---
+++ wk1/day3.js
@@ -0,0 +1,13 @@
+/*
+  Recreate the pop method without using .pop()
+  Remove and return the last item from a given array
+  After removing an item from an array, what else changes?
+*/
+
+/* ******************************************************************* */
+
+/*
+  Given an array and an item to search for,
+  return the index where the item is found,
+  return -1 to represent not found
+  */
