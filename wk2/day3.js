b8320720afeae9e750fff8f1abc1e3b35ecc8a71
Jason M Brady <jasonmbrady42@gmail.com>
2021-04-06 10:01:44 +0300

    day3 algos

--- wk2/day3.js
+++ wk2/day3.js
@@ -2,6 +2,14 @@
 
 function rFib(idx){
     //code goes here
+    //base case
+    if(idx === 0){
+        return 0;
+    }
+    if(idx === 1){
+        return 1;
+    }
+    return rFib(num - 1) + rFib(num - 2);
 }
 
 
@@ -10,6 +18,10 @@ function rFib(idx){
   return the array after reversing it
   Do it in place without using any built in methods
 */
+
+function reverse(arr){
+    //code
+}
 
 /* ********************************************************** */
 
@@ -20,3 +32,6 @@ function rFib(idx){
   the start index (inclusive) to the end index (exclusive)
   What should you do if the provided end index is out of bounds?
 */
+function slice(arr, start, end){
+    //code
+}
