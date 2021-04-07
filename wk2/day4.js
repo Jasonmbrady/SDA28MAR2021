f7643282cc612d90d7da06c692cf1993ef785a91
Jason M Brady <jasonmbrady42@gmail.com>
2021-04-07 08:32:54 +0300

    day 4

---
+++ wk2/day4.js
@@ -0,0 +1,34 @@
+/*
+  Given an array and an index,
+  remove the item at that index.
+  return the removed item
+  No built-in array methods except pop().
+*/
+
+// const arr2 = ["a", "b", "c"]
+// const removeIdx2 = 3
+// const expected2 = null
+// const arr2Expected = ["a", "b", "c"]
+
+// const arr3 = ["a", "b", "c"]
+// const removeIdx3 = -3
+// const expected3 = null
+// const arr3Expected = ["a", "b", "c"]
+
+function removeAt(arr, idx) {
+    // code here
+}
+/* ****************************************** */
+
+/*
+  Given an array, move the minimum value to the front
+  return the array after done
+  No built in methods
+*/
+
+// const nums1 = [1, 5, 2, 9];
+// const expected1 = [1, 5, 2, 9];
+
+function minToFront(nums) {
+    // code here
+}
