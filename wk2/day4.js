01eacc3e7873027cdcf835453b8aa7617d5bb22e
Jason M Brady <jasonmbrady42@gmail.com>
2021-04-07 09:44:18 +0300

    day4 solutions

--- wk2/day4.js
+++ wk2/day4.js
@@ -17,6 +17,15 @@
 
 function removeAt(arr, idx) {
     // code here
+    if(idx <0 || idx >= arr.length){
+        return null;
+    } // check to ensure idx is a valid index of arr
+    var value = arr[idx]; // store value
+    for(let i=0; i < arr.length; 1++){ // iterate through array
+        arr[i] = arr[i+1]; // shift values of arr forward by 1
+    }
+    arr[arr.length -1] = value; // set last element to value to be  removed
+    return arr.pop();
 }
 
 /* ****************************************** */
@@ -34,4 +43,26 @@ function removeAt(arr, idx) {
 
 function minToFront(nums) {
     // code here
+    if (nums.length === 0){
+        return [];
+    } // empty array check
+
+    var min = nums[0]; // initialize to first value and index
+    var mindx = 0;
+    for(let i=1; i<nums.length; i++){ // iterate through array
+        if (nums[i] < min){
+            min = nums[i];
+            mindx = i; // if a new minimum is found, set trackers
+        }
+    }
+    if (mindx === 0){
+        return nums; // check if first value is smallest
+    }
+    for(let i = mindx; i > 0; i--) {// start at mindx and iterate backward through array
+            nums[i] = nums[i-1]; // shift values toward end
+    }
+    nums[0] = min;
+    return nums;
+
+ 
 }
