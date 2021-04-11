9836a09e0e6edea0da725b78f2e427465ebc2256
Jason M Brady <jasonmbrady42@gmail.com>
2021-04-11 09:36:44 +0300

    day1 Algos added

---
+++ wk3/day1.js
@@ -0,0 +1,33 @@
+/*
+  String: Reverse
+  Given a string,
+  return a new string that is the given string reversed
+*/
+
+// const str1 = "creature"
+// const expected1 = "erutaerc"
+
+// const str2 = "maple"
+// const expected2 = "elpam"
+
+function reverseStr(){
+    //your code here
+}
+/* ************************************************* */
+
+/*
+  Acronyms
+  Create a function that, given a string, returns the string’s acronym
+  (first letter of each word capitalized).
+  Do it with .split first if you need to, then try to do it without
+*/
+
+// const str1 = " there's no free lunch - gotta pay yer way. ";
+// const expected1 = "TNFL-GPYW";
+
+// const str2 = "Live from New York, it's Saturday Night!";
+// const expected2 = "LFNYISN";
+
+function acronym(){
+    // your code here
+}
