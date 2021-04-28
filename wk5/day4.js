962120a15fdbcc43b141ad960ecb7340e29ba079
Jason M Brady <jasonmbrady42@gmail.com>
2021-04-28 11:00:07 +0300

    wk5 day4 solutions

--- wk5/day4.js
+++ wk5/day4.js
@@ -11,21 +11,45 @@
 //   penny: 1,
 // }
 
-// const cents1 = 25
+const cents1 = 25
 // const expected1 = { quarter: 1 }
 
-// const cents2 = 50
+ const cents2 = 50
 // const expected2 = { quarter: 2 }
 
-// const cents3 = 9
+const cents3 = 9
 // const expected3 = { nickel: 1, penny: 4 }
 
-// const cents4 = 99
+ const cents4 = 99
 // const expected4 = { quarter: 3, dime: 2, penny: 4 }
 
+// % Operator, divides two values returns the remainder 10%3 = 1
+
 function fewestCoinChange(cents) {
-    // Your Code Here
+    let changeBack = {};
+    let remaining = cents;
+    if (Math.floor(remaining/25) > 0){
+        changeBack.quarter = Math.floor(remaining/25);
+        remaining = remaining % 25;
+    }
+    if (Math.floor(remaining/10) > 0){
+        changeBack.dime = Math.floor(remaining/10);
+        remaining = remaining % 10;
+    }
+    if (Math.floor(remaining/5) > 0){
+        changeBack.nickel = Math.floor(remaining/5);
+        remaining = remaining % 5;
+    }
+    if (remaining > 0){
+        changeBack.penny = remaining;
+    }
+    return changeBack;
 }
+
+console.log(fewestCoinChange(cents1));
+console.log(fewestCoinChange(cents2));
+console.log(fewestCoinChange(cents3));
+console.log(fewestCoinChange(cents4));
 
 /* ******************************************************************************** */
 
@@ -36,13 +60,13 @@ function fewestCoinChange(cents) {
   Quickly determine and return the missing value.
 */
 
-// const nums1 = [3, 0, 1];
+ const nums1 = [3, 0, 1];
 // const expected1 = 2;
 
-// const nums2 = [3, 0, 1, 2];
+ const nums2 = [3, 0, 1, 2];
 // const expected2 = null;
 // Explanation: nothing is missing
-
+ const nums3 = [0,1,6,5,3,2];
 /*
   Bonus: now the lowest value can now be any integer (including negatives),
   instead of always being 0.
@@ -55,5 +79,19 @@ function fewestCoinChange(cents) {
 // const expected4 = 6;
 
 function missingValue(nums) {
-        // Your Code Here
+    let sum = 0;
+    let arrSum = 0;
+    for(let i=0; i < nums.length; i++){
+        sum += i;
+        arrSum += nums[i];
+    }
+    if (sum - arrSum == 0){
+        return null;
+    } else {
+        sum += nums.length;
+        return sum - arrSum;
+    }
 }
+console.log(missingValue(nums1));
+console.log(missingValue(nums2));
+console.log(missingValue(nums3));
