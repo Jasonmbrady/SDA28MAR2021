b25d88762af4fc58562fbd1ba5651a0171505d1e
Jason M Brady <jasonmbrady42@gmail.com>
2021-05-26 09:47:35 +0300

    added recursion prompt

--- wk7/day4.js
+++ wk7/day4.js
@@ -36,6 +36,19 @@ const nums3 = [1, 17, 12, 3, 9, 13, 21, 4, 27];
  *    being processed.
  * returns {Array<number>} The idx where left section of smaller items ends.
  */
-function partition(nums, left, right) {
+function partition(nums, left, right){
     // YOUR CODE HERE
 }
+
+// RECURSION AKA Quick Sort
+
+function quickSort(nums, left = 0, right = nums.length-1){
+    //YOUR CODE HERE
+}
+
+// quickSort(nums1);
+// quickSort(nums2);
+// quickSort(nums3);
+// console.log(nums1);
+// console.log(nums2);
+// console.log(nums3);
