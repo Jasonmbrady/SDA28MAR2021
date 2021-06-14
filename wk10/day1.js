287bfab27642994723a6279a34faff341d1f2f13
Jason M Brady <jasonmbrady42@gmail.com>
2021-06-14 09:31:48 +0300

    wk10 day2

--- wk10/day1.js
+++ wk10/day1.js
@@ -7,12 +7,12 @@
   Return an empty array if the callback never returns true
 */
 
-// const nums = [1, 4, 3, 6, 9, 15]
+// const nums = [1, 4, 3, 6, 9, 2, 15]
 
 // const callback1 = (elem) => {
 //   return elem > 5
 // }
-// const expected1 = [6, 9, 15]
+// const expected1 = [6, 9, 2, 15]
 
 // const callback2 = (elem) => {
 //   return elem > 2
@@ -22,9 +22,18 @@
 // const callback3 = (elem) => false
 // const expected3 = []
 
+// Time O(n)
+// Space O(1)
+
 function dropIt(arr, callback) {
-    // YOUR CODE HERE
+    for(let i = 0; i < arr.length; i++){
+      if(callback(arr[i])){
+        return arr.slice(i);
+      }
+    }
+    return [];
 }
+
 
 /*****************************************************************************/
 
@@ -58,4 +67,17 @@ function dropIt(arr, callback) {
 
 function canStringBecomePalindrome(str) {
   // YOUR CODE HERE
+  if (str.length === 0) {
+    return false;
+  }
+  const charMap = {};
+
+  for (const char of str) {
+    if (charMap.hasOwnProperty(char)){
+      delete charMap[char];
+    } else {
+      charMap[char] = true;
+    }
+    return Object.keys(charMap).length <= 1 ? true : false;
+  }
 }
