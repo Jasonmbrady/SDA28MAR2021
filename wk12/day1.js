f6b52352ac6f1d446d770d5b1a4bd755a19934d2
Jason M Brady <jasonmbrady42@gmail.com>
2021-06-27 09:09:41 +0300

    stacks

---
+++ wk12/day1.js
@@ -0,0 +1,32 @@
+class Stack {
+    // Last In First Out
+  
+    /**
+     * The constructor is executed when instantiating a new Stack() to construct
+     * a new instance.
+     * @return {Stack} This new Stack instance is returned without having to
+     *    explicitly write 'return' (implicit return).
+     */
+    constructor() {
+      this.items = []
+    }
+  
+    // methods go here
+
+    push(x) {
+      // Your Code Here
+    }
+    pop() {
+      // Your Code Here
+    }
+    isEmpty() {
+        // Your Code Here
+    }
+    size() {
+        // Your Code Here
+    }
+    peek() {
+        // Your Code Here
+    }
+  }
+  
