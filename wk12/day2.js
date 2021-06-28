b71906a2675413f43a80bff611c6b83f5faa026f
Jason M Brady <jasonmbrady42@gmail.com>
2021-06-28 09:05:19 +0300

    wk12 day2

---
+++ wk12/day2.js
@@ -0,0 +1,28 @@
+class Queue {
+    constructor() {
+      // 0 is the front
+      // arr.length-1 is the back
+      this.items = []
+    }
+  
+    enqueue (val) {
+    // Add a value to the back of Queue and return new length
+    }
+  
+    dequeue () {
+      // Remove and return value from front
+    }
+  
+    isEmpty () {
+      // return T if empty, F if not
+    }
+  
+    size () {
+      // return number of items in the Queue
+    }
+  
+    front () {
+      // Like Peek, returns the value of the item in the front without removing it from the queue.xs
+    }
+  
+  }
