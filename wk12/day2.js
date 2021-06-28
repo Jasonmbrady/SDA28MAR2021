5b191164323718c31ea10a88887d46f78c9240f5
Jason M Brady <jasonmbrady42@gmail.com>
2021-06-28 09:53:50 +0300

    wk12 day2 SLL solution

--- wk12/day2.js
+++ wk12/day2.js
@@ -1,28 +1,67 @@
+class Node {
+    constructor(data) {
+      this.data = data
+      this.next = null
+    }
+  }
+
 class Queue {
     constructor() {
       // 0 is the front
       // arr.length-1 is the back
-      this.items = []
+      this.head = null;
     }
   
     enqueue (val) {
     // Add a value to the back of Queue and return new length
+        if (this.isEmpty()){
+            this.head = new Node(val);
+        } else {
+            let runner = this.head;
+            while (runner.next !== null){
+                runner = runner.next;
+            }
+            runner.next = new Node(val);
+        }
     }
   
     dequeue () {
+        if (this.isEmpty()){
+            return null;
+        } else {
+            let dequeued = this.head;
+            this.head = this.head.next;
+            return dequeued.data;
+        }
       // Remove and return value from front
+
     }
   
     isEmpty () {
       // return T if empty, F if not
+        return this.head === null;
     }
   
     size () {
       // return number of items in the Queue
+      if (this.isEmpty()){
+          return 0;
+      } else {
+          let counter = 1;
+          let runner = this.head;
+          while (runner.next !== null){
+              counter++;
+              runner = runner.next;
+          }
+          return counter;
+      }
+
     }
   
     front () {
-      // Like Peek, returns the value of the item in the front without removing it from the queue.xs
+      // Like Peek, returns the value of the item in the front without removing it from the queue.
+      return this.head.data;
+
     }
   
   }
