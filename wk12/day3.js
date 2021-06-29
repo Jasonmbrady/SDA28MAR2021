f768fb1db7904d5cf6d6d95821aea209a326ee68
Jason M Brady <jasonmbrady42@gmail.com>
2021-06-29 10:20:42 +0300

    wk12 day 3 solutions

--- wk12/day3.js
+++ wk12/day3.js
@@ -3,7 +3,7 @@ class Node {
       this.data = data
       this.next = null
     }
-  }
+}
 
 class Queue {
     constructor() {
@@ -59,11 +59,78 @@ class Queue {
     compareQueues(q) {
       // - [ ] compareQueues
       //   - Write a method on the Queue class that, given another queue, will return whether they are equal (same items in same order).
+      if (this.size() !== q.size()){
+        return false;
+      } 
+      let counter = this.size();
+      while (counter > 0){
+        if (this.head.data !== q.head.data){
+          return false;
+        }
+        this.enqueue(this.dequeue());
+        q.enqueue(q.dequeue());
+        counter--;
+      }
+      return true;
+      }
+      
+  queueIsPalindrome() {
+    if(this.isEmpty){
+      return false;
+    } else if (this.head.next === null){
+      return true;
+    } else {
+      const reverse = new Stack();
+      let runner = this.head;
+      while (runner !== null){
+        reverse.push(runner.data);
+        runner = runner.next;
+      }
+      runner = this.head;
+      let value;
+      while (runner !== null){
+        value = reverse.pop();
+        if (runner.data !== value){
+          return false;
+        }
+        runner = runner.next;
+      }
+      return true;
     }
+  }
+}
 
-      queueIsPalindrome() {
-    // - [ ] queueIsPalindrome
-    //   - Write a method on the Queue class that returns whether or not the queue is a palindrome
-    
+class Stack {
+  constructor(){
+    this.head = null;
+  }
+  isEmpty(){
+    return this.head === null;
+  }
+
+  push(val){
+    if (this.isEmpty()){
+      this.head = new Node(val);
+    } else {
+      let runner = this.head;
+      while (runner.next !== null){
+        runner = runner.next;
+      }
+      runner.next = new Node(val);
+    }
+  }
+
+  pop(){
+    if (this.isEmpty()){
+      return null;
+    } else {
+      let runner = this.head;
+      while (runner.next !== null){
+        runner = runner.next;
+      }
+      const popped = runner.next;
+      runner.next = null;
+      return popped.data;
+    }
   }
 }
